9a701cefcee8c98fd49d246d0e1ee9297242aaa6
Sanketsaraf <86824468+Sanketsaraf@users.noreply.github.com>
2024-11-05 17:57:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use("/api/v1/post", require("./routes/postRoutes"));
 //home
 app.get("/", (req,res) => {
   res.status(200).send({
-    "success" = true,
+    "success": true,
     "message": "Node server running" 
   })
 })
